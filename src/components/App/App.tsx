154b36a65334f751e1cdb9c71e6fda7f32717253
Coconyan <San3k19@gmail.com>
2023-03-29 15:44:09 +0700

    init cra

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Lets start to make Snake Game
+      </header>
+    </div>
+  );
+}
+
+export default App;
